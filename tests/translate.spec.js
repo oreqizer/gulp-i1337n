efe4b0d7cdc6ae2a3e31df6bdf9f3d122c4feca6
oreqizer <oreqizer@gmail.com>
2017-07-27 13:17:57 +0200

    INIT

---
+++ tests/translate.spec.js
@@ -0,0 +1,34 @@
+const test = require("tape");
+
+const translate = require("../lib/translate").translate;
+
+const code = `
+  function asd() {
+    const v = __('kek');
+    const k = t('lol');
+    return v.toUpperCase();
+  }
+`;
+
+test("translate - base", t => {
+  const res = translate(code, { kek: "bur" }, {}).code;
+
+  t.ok(res.includes("bur"), "key was not translated");
+  t.ok(res.includes("lol"), "key was accidentally translated");
+  t.ok(!res.includes("__("), "func was not replaced");
+  t.end();
+});
+
+test("translate - custom func", t => {
+  const res = translate(code, { lol: "rofl" }, { func: "t" }).code;
+
+  t.ok(res.includes("kek"), "key was accidentally translated");
+  t.ok(res.includes("rofl"), "key was not translated");
+  t.ok(!res.includes("t("), "func was not replaced");
+  t.end();
+});
+
+test("translate - strict", t => {
+  t.throws(() => translate(code, {}, { strict: true }), /missing translation/);
+  t.end();
+});
