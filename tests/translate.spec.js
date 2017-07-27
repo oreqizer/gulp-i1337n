f347f7f51e1010c52419ed03db16f58397ea827d
oreqizer <oreqizer@gmail.com>
2017-07-27 13:48:58 +0200

    translate: More robust test

--- tests/translate.spec.js
+++ tests/translate.spec.js
@@ -1,39 +1,30 @@
+/* eslint-disable no-eval */
 const test = require("tape");
 
 const translate = require("../src/translate");
 
-const code = `
-  function asd() {
-    const v = __('kek');
-    const k = t('lol');
-    return v.toUpperCase();
-  }
-`;
-
 test("translate - base", t => {
-  const res = translate(code, { kek: "bur" }, {}).code;
+  const res = translate("__('kek');", { kek: "bur" }, {}).code;
 
-  t.ok(res.includes("bur"), "key was not translated");
-  t.ok(res.includes("lol"), "key was accidentally translated");
+  t.equal(eval(res), "bur", "key was not translated");
   t.ok(!res.includes("__("), "func was not replaced");
   t.end();
 });
 
 test("translate - custom func", t => {
-  const res = translate(code, { lol: "rofl" }, { func: "t" }).code;
+  const res = translate("t('kek');", { kek: "bur" }, { func: "t" }).code;
 
-  t.ok(res.includes("kek"), "key was accidentally translated");
-  t.ok(res.includes("rofl"), "key was not translated");
+  t.equal(eval(res), "bur", "key was not translated");
   t.ok(!res.includes("t("), "func was not replaced");
   t.end();
 });
 
 test("translate - strict", t => {
-  t.throws(() => translate(code, {}, { strict: true }), /missing translation/);
+  t.throws(() => translate("__('kek');", {}, { strict: true }), /Missing translation/);
   t.end();
 });
 
 test("translate - bad value", t => {
-  t.throws(() => translate(code, { kek: null }, {}), /a string/);
+  t.throws(() => translate("__('kek');", { kek: null }, {}), /a string/);
   t.end();
 });
